971242c80d4c7da7709e77ab1d33dd46c74717e9
張騰升mts <momo16542@gmail.com>
2019-11-04 13:40:56 +0800

    1

---
+++ DiceGame/Dice6ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    class Dice6ViewModel : ViewModelBase
+    {
+
+       
+        public Dice6ViewModel()
+        {
+            
+        }
+    }
+}
